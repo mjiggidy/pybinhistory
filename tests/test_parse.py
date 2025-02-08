f28d169f84f210ceb3a7a61e6e49d7d6b10f0df8
Michael Jordan <michael@glowingpixel.com>
2025-02-07 18:17:33 -0800

    Verify output

--- tests/test_parse.py
+++ tests/test_parse.py
@@ -1,7 +1,30 @@
 import sys
 from binlog import BinLog
 
-log = BinLog.from_filepath(sys.argv[1])
+good = 0
+bad = 0
 
-for entry in log.entries:
-	print(entry)
+for path_log in sys.argv[1:]:
+	
+	log = BinLog.from_filepath(path_log)
+
+	with open(path_log) as handle:
+		orig = handle.read()
+
+	if log.to_string() != orig:
+		
+		print(path_log)
+
+		print(log.to_string())
+		print()
+		print(orig)
+		print()
+		print(log.to_string() == orig)
+
+		bad += 1
+	
+	else:
+		good += 1
+
+print(f"Good: {good},   Bad: {bad}")
+	
