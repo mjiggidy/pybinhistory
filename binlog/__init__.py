3133aa9dc31acf91e7191bc51822b3b58303df45
mjordan <michael@glowingpixel.com>
2025-02-19 22:02:48 -0800

    Add exceptions

--- binlog/__init__.py
+++ binlog/__init__.py
@@ -1,8 +1,8 @@
 MAX_ENTRIES:int = 10
 """Maximum log entries allowed in a file"""
 
-DEFAULT_FILE_EXTENSION = ".log"
+DEFAULT_FILE_EXTENSION:str = ".log"
 """The expected file extension for bin log files"""
 
-from .exceptions import BinLogParseError
+from .exceptions import BinLogParseError, BinLogFieldLengthError
 from .binlog import BinLog, BinLogEntry
--- binlog/exceptions.py
+++ binlog/exceptions.py
@@ -1,2 +1,5 @@
 class BinLogParseError(ValueError):
 	"""An invalid value was encountered while parsing the log"""
+
+class BinLogFieldLengthError(ValueError):
+	"""A log field is not a valid length (between 1 and MAX_FIELD_LENGTH chars)"""
