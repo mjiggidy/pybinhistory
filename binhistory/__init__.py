1dae3573ac623c7126005df68578f3938ee32918
Michael Jordan <michael@glowingpixel.com>
2025-03-10 18:52:56 -0700

    Protect the modules-ish

--- binhistory/__init__.py
+++ binhistory/__init__.py
@@ -1,6 +1,12 @@
 """
-:mod:`binhistory` is for working with Avid log files.
+:mod:`binhistory` is for working with Avid bin ``.log`` files.
+
+It is designed to help developers read and write valid bin logs 
+without a whole lotta fuss.
+
+Tenderly over-engineered by Michael Jordan <michael@glowingpixel.com> while he struggled to find his next AE gig.
 """
 
 from . import exceptions, defaults
-from .binlog import BinLog, BinLogEntry
+from ._binlog import BinLog
+from ._binlogentry import BinLogEntry
